b36e56b79e078a48ac1a931571addd135fefc053
shashank <theshashank6870@gmail.com>
2020-05-23 20:55:36 +0530

    Validation view added

--- ajaxpractice/urls.py
+++ ajaxpractice/urls.py
@@ -2,11 +2,12 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from users.views import SignUpView
+from users.views import SignUpView,validate_username
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('signup', SignUpView.as_view(), name='signup'),
+    path('signup/', SignUpView.as_view(), name='signup'),
+    path('ajax/validate_username/', validate_username, name='validate_username'),
 ] 
 
 if settings.DEBUG:
