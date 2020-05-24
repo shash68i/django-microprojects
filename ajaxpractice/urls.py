b82fec054d23403a907a27a58eeadbc023ab8113
shashank <theshashank6870@gmail.com>
2020-05-24 23:31:41 +0530

    Fully working code with no optimizations

--- ajaxpractice/urls.py
+++ ajaxpractice/urls.py
@@ -2,13 +2,14 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from users.views import SignUpView,validate_username
+from users.views import SignUpView, validate_username
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('signup/', SignUpView.as_view(), name='signup'),
     path('ajax/validate_username/', validate_username, name='validate_username'),
-] 
+]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
